b02403737518a2bbc5d2300e305ac2c6441ea88f
Chris Tensmeyer <Christopher.tensmeyer@gmail.com>
2015-11-20 11:12:11 -0700

    Converted tabs to spaces

--- create_dataset.py
+++ create_dataset.py
@@ -72,13 +72,13 @@ def createCrops(num_crops, root, in_file, out_folder):
     filenames = loadFileNames(in_file, root)
     
     crops = []
-	images = []
+    images = []
 
-	for fn in filenames:
-		original_im = io.imread(fn)
-		for scale in IMAGE_SCALES:
-			resized_im = transform.rescale(original_im, scale)
-			images.append(resized_im)
+    for fn in filenames:
+        original_im = io.imread(fn)
+        for scale in IMAGE_SCALES:
+            resized_im = transform.rescale(original_im, scale)
+            images.append(resized_im)
 
     print "Generating %d crops:" % (num_crops)
 
@@ -116,38 +116,38 @@ def main(args):
     crops = getImages(args.num_crops, args.root, args.data_manifest, args.crop_out_dir, args.crops_no_cache, createCrops, "crops")
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Creates a dataset")
-	parser.add_argument("-r", "--root", default='data', 
-				help="All filepaths are relative to the root")
-	parser.add_argument("--sprite-in-file", default="data/original_sprites.txt", 
-				help="File containing paths to each sprite")
-	parser.add_argument("--sprite-out-dir", default="data/images/augmented_sprites",
-				help="Output directory where the augmented sprites will be written")
-	parser.add_argument("--sprite-no-cache", default=False, action="store_true",
-				help="By default use any precomputed augmented sprites")
-	parser.add_argument("-s", "--num-sprites", default=20, type=int, 
-				help="Number of random occlusions applied to each sprite")
+    parser = argparse.ArgumentParser(description="Creates a dataset")
+    parser.add_argument("-r", "--root", default='data', 
+                help="All filepaths are relative to the root")
+    parser.add_argument("--sprite-in-file", default="data/original_sprites.txt", 
+                help="File containing paths to each sprite")
+    parser.add_argument("--sprite-out-dir", default="data/images/augmented_sprites",
+                help="Output directory where the augmented sprites will be written")
+    parser.add_argument("--sprite-no-cache", default=False, action="store_true",
+                help="By default use any precomputed augmented sprites")
+    parser.add_argument("-s", "--num-sprites", default=20, type=int, 
+                help="Number of random occlusions applied to each sprite")
 
-	parser.add_argument("-d", "--data-manifest", default='data/training_images.txt', 
-				help="File containing paths to each training image")
-	parser.add_argument("--crop-out-dir", default="data/images/random_crops",
-				help="Output directory where the random crops are put")
-	parser.add_argument("-n", "--num-crops", default=10000, type=int, 
-				help="Number of random crops taken from each training image")
-	parser.add_argument("--crops-no-cache", default=False, action="store_true",
-				help="By default use previously computed random crops")
+    parser.add_argument("-d", "--data-manifest", default='data/training_images.txt', 
+                help="File containing paths to each training image")
+    parser.add_argument("--crop-out-dir", default="data/images/random_crops",
+                help="Output directory where the random crops are put")
+    parser.add_argument("-n", "--num-crops", default=10000, type=int, 
+                help="Number of random crops taken from each training image")
+    parser.add_argument("--crops-no-cache", default=False, action="store_true",
+                help="By default use previously computed random crops")
 
-	parser.add_argument("-p", "--percent-positive", default=0.5, type=float, 
-				help="Percentage of random crops that will be positive examples")
-	parser.add_argument("-o", "--training-out-dir", default="data/images/training_data",
-				help="Output directory where the random crops are put")
-	parser.add_argument("-m", "--training-manifest", default="data/images/training_manifest.txt",
-				help="Output Manifest File listing training images and labels")
+    parser.add_argument("-p", "--percent-positive", default=0.5, type=float, 
+                help="Percentage of random crops that will be positive examples")
+    parser.add_argument("-o", "--training-out-dir", default="data/images/training_data",
+                help="Output directory where the random crops are put")
+    parser.add_argument("-m", "--training-manifest", default="data/images/training_manifest.txt",
+                help="Output Manifest File listing training images and labels")
 
-	return parser.parse_args()
-	
+    return parser.parse_args()
+    
 
 if __name__ == "__main__":
-	args = parse_args()
-	main(args)
+    args = parse_args()
+    main(args)
 
