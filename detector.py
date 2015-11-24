70c5ba22a4f6ed0abf37f7c2b7dcf7c19109a063
Chris Tensmeyer <Christopher.tensmeyer@gmail.com>
2015-11-23 17:57:36 -0700

    detector script

---
+++ detector.py
@@ -0,0 +1,158 @@
+
+import os
+import sys
+import cv2
+import caffe
+import numpy as np
+from PIL import Image, ImageDraw
+import argparse
+
+
+IMG_SCALES = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3]
+
+def init_caffe(args):
+	if args.gpu >= 0:
+		caffe.set_mode_gpu()
+		caffe.set_device(args.gpu)
+	else:
+		caffe.set_mode_cpu()
+
+	caffenet = caffe.Net(args.caffe_model, args.caffe_weights, caffe.TEST)
+	transformer = caffe.io.Transformer({args.input: caffenet.blobs[args.input].data.shape}, resize=True)
+	transformer.set_transpose(args.input, (2,0,1))
+	if args.mean_file:
+		transformer.set_mean(args.input, np.load(args.mean_file).mean(1).mean(1))
+	else:
+		transformer.set_mean(args.input, np.asarray([-1 * args.shift] * 3))
+	transformer.set_raw_scale(args.input, args.scale)
+
+	transformer.set_channel_swap(args.input, (2,1,0))
+	caffenet.transformer = transformer
+
+	return caffenet
+
+
+def get_im_files(args):
+	img_root = args.img_dir
+	im_files = list()
+	for line in open(args.manifest, 'r').readlines():
+		line = line.rstrip()
+		im_files.append(os.path.join(img_root, line))
+
+	return im_files
+
+
+def detect(im, caffenet, args):
+	sh = im.shape
+	heatmap = np.zeros( (sh[0] / args.stride + 1, sh[1] / args.stride + 1) )
+	bbs = []
+
+	detect_height = caffenet.blobs[args.input].data.shape[2]
+	detect_width = caffenet.blobs[args.input].data.shape[3]
+	for y in xrange(0, sh[0], args.stride):
+		for x in xrange(0, sh[1], args.stride):
+			win = im[y:y+detect_height, x:x+detect_width,:]
+			win = caffenet.transformer.preprocess(args.input, win)
+
+			caffenet.blobs[args.input].data[...] = win
+			caffenet.forward()
+			heatmap[y / args.stride, x / args.stride] = caffenet.blobs[args.output_layer].data.flatten()[1]
+
+	return heatmap, bbs
+
+
+def get_output_name(f, out_dir, tag):
+	base = os.path.basename(f)
+	ext = base[-4:]
+	base = base[:-4]
+
+	return os.path.join(out_dir, "%s_%s%s" % (base, tag, ext))
+
+
+def draw_bb(draw, x, y, width, height, color='blue', thick=5):
+	for t in xrange(thick):
+		draw.rectangle( [x-t, y-t, x + width + t, y + height + t], outline=color)
+			
+
+def main(args):
+	caffenet = init_caffe(args)
+	im_files = get_im_files(args)
+	detect_height = caffenet.blobs[args.input].data.shape[2]
+	detect_width = caffenet.blobs[args.input].data.shape[3]
+
+	try:
+		os.makedirs(args.out_dir)
+	except:
+		pass
+
+	for x, f in enumerate(im_files):
+		try:
+			im = Image.open(f)
+			im = im.convert(mode='RGB')
+
+			im_np = np.array(im.getdata()).reshape(im.size[1], im.size[0], 3)
+
+			heatmap, bbs = detect(im_np, caffenet, args)
+			heat_out = get_output_name(f, args.out_dir, "heatmap")
+			bb_out = get_output_name(f, args.out_dir, "bb")
+			print f, im_np.shape, heatmap.shape
+			
+			y, x = np.unravel_index(heatmap.argmax(), heatmap.shape)
+			print x, y
+			x = x * args.stride
+			y = y * args.stride
+
+			heatmap = (heatmap / heatmap.max() * 255).astype(dtype=np.uint8)
+			heatmap = cv2.resize(heatmap, (im_np.shape[1], im_np.shape[0]), interpolation=cv2.INTER_NEAREST)
+			print heatmap.shape
+			cv2.imwrite(heat_out, heatmap)
+
+			draw = ImageDraw.Draw(im)
+			draw_bb(draw, x, y, detect_width, detect_height)
+			im.save(bb_out)
+
+
+		except:
+			print "Exception occured processing %s" % f
+			raise
+
+
+def get_args():
+	parser = argparse.ArgumentParser(description="Detection with Caffe")
+	parser.add_argument("caffe_model", 
+				help="The model definition file (e.g. deploy.prototxt)")
+	parser.add_argument("caffe_weights", 
+				help="The model weight file (e.g. net.caffemodel)")
+	parser.add_argument("manifest", 
+				help="File listing image paths relative to img_dir.  Determines order of features")
+	parser.add_argument("img_dir", 
+				help="Root directory for the images")
+	parser.add_argument("out_dir", 
+				help="Root directory for the output images")
+
+	
+	parser.add_argument("--stride", type=int, default=15,
+				help="Stride of the sliding window")
+	parser.add_argument("--output-layer", type=str, default="probs",
+				help="Name of the output layer")
+
+	parser.add_argument("-m", "--mean-file", type=str, default="",
+				help="Optional mean file for input normalization")
+	parser.add_argument("--gpu", type=int, default=-1,
+				help="GPU to use for running the network")
+	parser.add_argument("-a", "--scale", type=float, default=1.0,
+				help="Optional scale factor")
+	parser.add_argument("-b", "--shift", type=float, default=0.0,
+				help="Optional shift factor")
+	parser.add_argument("-i", "--input", type=str, default="data",
+				help="Name of input blob")
+	
+	return parser.parse_args()
+
+	
+	
+
+if __name__ == "__main__":
+	args = get_args()
+	main(args)
+
